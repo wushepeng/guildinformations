a0f24843a789957ce068d816436850711c0e7c0b
Wu She-Peng <wushepeng@gmx.fr>
2014-09-17 13:54:59 +0200

    Update creationbdd.sql

--- creationbdd.sql
+++ creationbdd.sql
@@ -1,24 +1,28 @@
 CREATE TABLE gi_general_config(
-RYAPI_APP_KEY varchar(?) NOT NULL PRIMARY KEY,
-RYAPI_APP_URL NOT NULL DEFAULT 'http://www.la-firme-matis.com/guildinformations/controleur.php',
+RYAPI_APP_KEY char(41) NOT NULL,
+RYAPI_APP_URL text NOT NULL,
 RYAPI_APP_MAXAGE integer NOT NULL DEFAULT 0,
-GUILDID integer
+PRIMARY KEY(RYAPI_APP_KEY)
 );
+
+CREATE TABLE gi_guilde_api(
+idguilde integer PRIMARY KEY,
+nomguilde varchar(50),
+apikey varchar(41),
+mainguild integer NOT NULL,
+FOREIGN KEY (mainguild) REFERENCES gi_guilde_api(idguilde)
+) ENGINE=INNODB;
 
 CREATE TABLE gi_perso_api( 
 idperso integer PRIMARY KEY,
 nomperso varchar(50),
-apikey varchar(?),
-);
-CREATE TABLE gi_guilde_api(
-idguilde integer PRIMARY KEY,
-nomguilde varchar(50),
-apikey varchar(?),
-);
+apikey varchar(41),
+mainguild integer NOT NULL,
+FOREIGN KEY (mainguild) REFERENCES gi_guilde_api(idguilde)
+) ENGINE=INNODB;
 
 CREATE TABLE gi_perso_config(
 idperso integer NOT NULL,
 codecomp varchar(15) NOT NULL,
 visible boolean NOT NULL DEFAULT true
 );
-
