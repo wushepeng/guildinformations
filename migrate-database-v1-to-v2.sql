c2950b65ae6700ea98f6a104f66f78f8751f4564
Eric Masoero <eric.masoero@gmail.com>
2015-07-04 00:29:43 +0200

    #4 peut-être script qui marche, à tester

--- migrate-database-v1-to-v2.sql
+++ migrate-database-v1-to-v2.sql
@@ -1,13 +1,16 @@
-DROP TABLE IF EXISTS `gi_GeneralConfig`;
-CREATE TABLE `gi_GeneralConfig` (
+DROP DATABASE IF EXISTS `guildInf_test`;
+CREATE DATABASE `guildInf_test`;
+
+DROP TABLE IF EXISTS `guildInf_test`.`gi_GeneralConfig`;
+CREATE TABLE `guildInf_test`.`gi_GeneralConfig` (
   `RYAPI_APP_KEY` varchar(41) NOT NULL,
   `RYAPI_APP_URL` text NOT NULL,
   `RYAPI_APP_MAXAGE` int(11) NOT NULL DEFAULT '0',
   UNIQUE KEY `RYAPI_APP_KEY` (`RYAPI_APP_KEY`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS `gi_Guilds`;
-CREATE TABLE `gi_Guilds` (
+DROP TABLE IF EXISTS `guildInf_test`.`gi_Guilds`;
+CREATE TABLE `guildInf_test`.`gi_Guilds` (
   `id` int(11) NOT NULL,
   `name` varchar(50) NOT NULL,
   `apiKey` varchar(41) DEFAULT NULL,
@@ -15,8 +18,8 @@ CREATE TABLE `gi_Guilds` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS `gi_Homins`;
-CREATE TABLE `gi_Homins` (
+DROP TABLE IF EXISTS `guildInf_test`.`gi_Homins`;
+CREATE TABLE `guildInf_test`.`gi_Homins` (
   `id` int(11) NOT NULL,
   `name` varchar(50) NOT NULL,
   `apiKey` varchar(41) DEFAULT NULL,
@@ -24,26 +27,15 @@ CREATE TABLE `gi_Homins` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-/* Dans l'ancienne base de donnés le champ nom, pour les guildes et les homins, peut être à null
- * Si c'est le cas, on ajoute un nom par défaut, le nom étant mis à jour par la nouvelle application
- */
-UPDATE `competences2`.`gi_guilde_api`
-SET `nomguilde` = "theNameWillBeUpdatedByTheApp"
-WHERE `nomguilde` IS NULL;
-
-UPDATE `competences2`.`gi_perso_api`
-SET `nomperso` = "theNameWillBeUpdatedByTheApp"
-WHERE `nomperso` IS NULL;
-
-INSERT INTO `gi_GeneralConfig`
+INSERT INTO `guildInf_test`.`gi_GeneralConfig`
 VALUES ('_APPKEY_', '_APPURL_', 0);
 
-INSERT INTO `gi_Guilds` (`id`, `name`, `apiKey`, `mainGuildId`)
+INSERT INTO `guildInf_test`.`gi_Guilds` (`id`, `name`, `apiKey`, `mainGuildId`)
 SELECT `g`.`idguilde`, `g`.`nomguilde`, `g`.`apikey`, `g`.`mainguild`
-FROM `competences2`.`gi_guilde_api` AS `g`
+FROM `competences`.`gi_guilde_api` AS `g`
 WHERE `g`.`idguilde` > 0;
 
-INSERT INTO `gi_Homins` (`id`, `name`, `apiKey`, `guildId`)
+INSERT INTO `guildInf_test`.`gi_Homins` (`id`, `name`, `apiKey`, `guildId`)
 SELECT `p`.`idperso`, `p`.`nomperso`, `p`.`apikey`, `p`.`mainguild`
-FROM `competences2`.`gi_perso_api` AS `p`
+FROM `competences`.`gi_perso_api` AS `p`
 WHERE `p`.`idperso` > 0;
