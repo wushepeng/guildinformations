d049f4c06186cc0f86c5ddc9edc0ed141a665c2d
Eric Masoero <eric.masoero@gmail.com>
2015-07-08 16:43:53 +0200

    #7 Début d'extraction des textes dans les templates pour la traduction

--- lang/fr/m.php
+++ lang/fr/m.php
@@ -1,7 +1,25 @@
 <?php
 
+/*
+French translations
+*/
+
 return array(
-    'welcome' => 'Bonjour'
+	// Layout template
+	'layout.mainTitle' => 'Informations de guilde',
+	'layout.ryzomApp' => 'Ryzom App',
+	'layout.home' => 'Accueil',
+	//Content template
+	'content.apiKey' => 'Clé api',
+	'content.guildApiKey' => 'Clé de guilde',
+	'content.warehouses' => 'Gérer les entrepôts de guilde',
+	'content.inventory' => 'Inventaire',
+	'content.skills' => 'Compétences des membres',
+	// Home template
+	'home.title' => 'Accueil',
+    'home.welcome' => 'Bonjour',
+    'home.noGuild' => "L'application est réservée aux membres d'une guilde.",
+    'home.backTo' => 'Retour sur'
 );
 
 ?>
