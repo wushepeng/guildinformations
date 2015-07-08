2782b22aec3c63bcd685d63042f95e08a69520f2
Eric Masoero <eric.masoero@gmail.com>
2015-07-08 21:09:31 +0200

    #7 Extraction des textes des templates hominKey, guildKey et guildConf

--- lang/fr/m.php
+++ lang/fr/m.php
@@ -19,7 +19,26 @@ return array(
 	'home.title' => 'Accueil',
     'home.welcome' => 'Bonjour',
     'home.noGuild' => "L'application est réservée aux membres d'une guilde.",
-    'home.backTo' => 'Retour sur'
+    'home.backTo' => 'Retour sur',
+    // Homin key template
+    'homin.apiKey' => 'Clé Api',
+    'homin.error' => 'Votre clé est invalide, veuillez la mettre à jour.',
+    'homin.giveKey' => 'Entrez votre clé api',
+    'homin.nota' => 'clé avec l\'option "P02 Compétences"',
+    // Guild key template
+    'guild.key.apiKey' => 'Clé Api de guilde',
+    'guild.key.giveKey' => 'Entrez votre clé api de guilde',
+    // Guild conf template
+    'guild.conf.mainTitle' => 'Gérer les entrepôts de guilde',
+    'guild.conf.title' => 'Entrepôts de guilde',
+    'guild.conf.error' => 'Problème lors de la récupération des informations. Vérifiez la clé api et re-essayez.',
+    'guild.conf.remove' => 'Supprimer',
+    'guild.conf.add' => 'Ajouter un nouvel entrepôt',
+    // Shared by guild key & conf templates
+    'guild.nota' => 'clé avec l\'option "G03 Inventaire"',
+    // Shared by homin key, guild key & conf templates
+    'btn.save' => 'Enregistrer',
+    'btn.maj' => 'Mettre à jour'
 );
 
 ?>
